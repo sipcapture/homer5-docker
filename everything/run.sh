74bf55ea4d6d2d0923212203ee218ac1ff34d19f
U-D-SHAUN\shaun <shaun@broadbent.cc>
2017-05-08 18:32:58 +1000

    Initial commit of changes to support running a single container

--- everything/run.sh
+++ everything/run.sh
@@ -71,9 +71,9 @@ PATH_HOMER_CONFIG=/var/www/html/api/configuration.php
 chmod 775 $PATH_HOMER_CONFIG
 
 # Replace values in template
-perl -p -i -e "s/\{\{ DB_PASS \}\}/$DB_PASS/" $PATH_HOMER_CONFIG
-perl -p -i -e "s/\{\{ DB_HOST \}\}/$DB_HOST/" $PATH_HOMER_CONFIG
-perl -p -i -e "s/\{\{ DB_USER \}\}/$DB_USER/" $PATH_HOMER_CONFIG
+perl -p -i -e "s/\{\{ DB_PASSWORD \}\}/$DB_PASS/" $PATH_HOMER_CONFIG
+perl -p -i -e "s/\{\{ DB_HOSTNAME \}\}/$DB_HOST/" $PATH_HOMER_CONFIG
+perl -p -i -e "s/\{\{ DB_USERNAME \}\}/$DB_USER/" $PATH_HOMER_CONFIG
 
 # Set Permissions for webapp
 mkdir /var/www/html/api/tmp
@@ -152,10 +152,13 @@ if [ "$DB_HOST" == "$DOCK_IP" ]; then
 
     # Reconfigure rotation
     export PATH_ROTATION_SCRIPT=/opt/homer_rotate
+    export PATH_ROTATION_INI_SCRIPT=/opt/rotation.ini
     chmod 775 $PATH_ROTATION_SCRIPT
     chmod +x $PATH_ROTATION_SCRIPT
     perl -p -i -e "s/homer_user/$DB_USER/" $PATH_ROTATION_SCRIPT
     perl -p -i -e "s/homer_password/$DB_PASS/" $PATH_ROTATION_SCRIPT
+    perl -p -i -e "s/homer_user/$DB_USER/" $PATH_ROTATION_INI_SCRIPT
+    perl -p -i -e "s/homer_password/$DB_PASS/" $PATH_ROTATION_INI_SCRIPT
     # Init rotation
     /opt/homer_rotate > /dev/null 2>&1
     
@@ -172,9 +175,9 @@ mv $PATH_KAMAILIO_CFG.tmp $PATH_KAMAILIO_CFG
 
 # Replace values in template
 perl -p -i -e "s/\{\{ LISTEN_PORT \}\}/$LISTEN_PORT/" $PATH_KAMAILIO_CFG
-perl -p -i -e "s/\{\{ DB_PASS \}\}/$DB_PASS/" $PATH_KAMAILIO_CFG
-perl -p -i -e "s/\{\{ DB_HOST \}\}/$DB_HOST/" $PATH_KAMAILIO_CFG
-perl -p -i -e "s/\{\{ DB_USER \}\}/$DB_USER/" $PATH_KAMAILIO_CFG
+perl -p -i -e "s/\{\{ HOMER_DB_PASSWORD \}\}/$DB_PASS/" $PATH_KAMAILIO_CFG
+perl -p -i -e "s/\{\{ HOMER_DB_HOST \}\}/$DB_HOST/" $PATH_KAMAILIO_CFG
+perl -p -i -e "s/\{\{ HOMER_DB_USER \}\}/$DB_USER/" $PATH_KAMAILIO_CFG
 
 # Change kamailio datestamp for sql tables
 # sed -i -e 's/# $var(a) = $var(table) + "_" + $timef(%Y%m%d);/$var(a) = $var(table) + "_" + $timef(%Y%m%d);/' $PATH_KAMAILIO_CFG
